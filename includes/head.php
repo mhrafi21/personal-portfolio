348d414a3d5be579e1c68af1ac086e0a4abe078a
12mhrafi <12mhrafi@gmail.com>
2022-09-22 18:44:57 +0600

    RE

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+
   <!--   font awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- font awesome -->
@@ -13,12 +13,12 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,800;0,900;1,600;1,700&display=swap" rel="stylesheet">
   <!-- Link Swiper's CSS -->
   <link rel="stylesheet" href="css/main.min.css">
+  <!-- custom css -->
   <link rel="stylesheet" href="styles/style.css">
-
-
-
-
-
+  <!-- favicon -->
+  <link rel="icon" href="img/favicon.ico" type = "image/x-icon">
+  <!-- favicon -->
+  <title>Rafi | Personal-portfolio</title>
 </head>
 
-<body class = "bg-light">
+<body class="bg-light">
--- portfolio.php
+++ portfolio.php
@@ -9,6 +9,21 @@
                     <h2 class="text-dark">Portfolio</h2>
                     <p class="text-muted">Recent Works</p>
                     <div class="row">
+                              <div class=" col-md-4">
+                                        <div class="card">
+                                                  <img src="./img/portfolio.jpg" class="card-img-top" alt="p" style="width:100%;height:15rem; object-fit:cover;object-position:center">
+                                                  <div>
+                                                            <h5>Ecommerce project</h5>
+                                                            <div class = "d-flex ">
+                                                                      <span><i class="fa-solid fa-check"></i><span>HTML</span></span>
+                                                                      <span><i class="fa-solid fa-check"></i><span>PHP</span></span>
+                                                                      <span><i class="fa-solid fa-check"></i><span>JS</span></span>
+                                                                      <span><i class="fa-solid fa-check"></i><span>CSS</span></span>
+                                                                      <span><i class="fa-solid fa-check"></i><span>Bootstrap</span></span>
+                                                            </div>
+                                                  </div>
+                                        </div>
+                              </div>
 
                     </div>
           </div>
